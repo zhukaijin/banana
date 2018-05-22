02f2ed7bd1eceb75eab52c5bba83946642171a11
zhu kaijin <gaidouxi@rrcs-172-254-99-29.nyc.biz.rr.com>
2018-05-23 01:37:36 +0900

    comment

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
-mongoose.connect("mongodb://localhost/nodejs-blog");
+mongoose.connect("mongodb://202.182.98.219/banana");
 let db = mongoose.connection;
 
 db.on('error',function(err){
@@ -16,6 +17,10 @@ db.once('open',function(){
 const app = express();
 let Article = require('./models/article');
 
+// create application/x-www-form-urlencoded parser
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname,'public')));
 app.set('views', path.join(__dirname,'views'));
 app.set('view engine','pug');
 app.get('/',function(req,res){
@@ -26,15 +31,73 @@ app.get('/',function(req,res){
   });
 });
 
+
 app.get('/layout',function(req,res){
   res.render('layout',{
     title:'Articles'
   });
 });
 
-app.get('/aticles/new',function(req,res){
-  res.render('index',{
+app.get('/articles/new',function(req,res){
+  res.render('new',{
     title:'New'
+  });
+});
+
+app.get('/articles/:id', function(req, res) {
+  Article.findById(req.params.id, function(err, article) {
+    res.render('show', {
+      article: article
+    });
+  });
+});
+
+app.post('/articles/update/:id', function(req, res) {
+  let query = { _id: req.params.id };
+
+  Article.update(query, req.body, function(err) {
+    if (err) {
+      console.log(err);
+      return;
+    } else {
+      res.redirect('/');
+    }
+  });
+});
+
+
+
+app.get('/articles/:id/edit', function(req, res) {
+  Article.findById(req.params.id, function(err, article) {
+    res.render('edit', {
+      title:'edit article',
+      article: article
+    });
+  });
+});
+
+app.post('/articles/create', function(req, res) {
+  let article = new Article(req.body);
+
+  article.save(function(err) {
+    if (err) {
+      console.log(err);
+      return;
+    } else {
+      res.redirect('/');
+    }
+  });
+});
+
+app.delete('/articles/:id', function(req, res) {
+  let query = { _id: req.params.id };
+
+  Article.remove(query, function(err) {
+    if (err) {
+      console.log(err);
+    }
+
+    res.send('Success');
   });
 });
 
